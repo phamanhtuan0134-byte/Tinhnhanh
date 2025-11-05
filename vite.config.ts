92ff875975c236488d1d10bb06a32d626c7bd9b9
phamanhtuan0134-byte <phamanhtuan0134@gmail.com>
2025-11-05 18:54:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Tinhnhanh6/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Tinhnhanh/' : '/',
   plugins: [react()],
 })
 
