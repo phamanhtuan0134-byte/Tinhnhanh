f64ac25d5aa9c6c7fb4ee24819194ba30d84ebc6
phamanhtuan0134-byte <phamanhtuan0134@gmail.com>
2025-11-05 18:39:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/Tinhnhanh6/' : '/',
+  plugins: [react()],
+})
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
